22ab9b1137ed9fd0f31db8274859f53e438e264e
jaber1104076 <jabercuet11@gmail.com>
2022-11-27 15:48:45 +0600

    delete added

--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../contexts/ContextProvider';
 import SmallSpinner from '../../Shared/Spinner/SmallSpinner';
 
 
 const MyProducts = () => {
     const { user } = useContext(AuthContext)
+    const [advertiseId, setAdvertiseID] = useState('')
+    const [advertise, setAdvertise] = useState('')
     const { data: products, isLoading, refetch = [] } = useQuery({
         queryKey: ['myproducts', user?.email],
         queryFn: async () => {
@@ -15,17 +18,61 @@ const MyProducts = () => {
         }
     })
 
+    useEffect(() => {
+        fetch(`http://localhost:5000/myproducts/${advertiseId}`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                setAdvertise(data)
+            })
+    }, [advertiseId])
+
     if (isLoading) {
         return <SmallSpinner></SmallSpinner>
     }
 
     const handleAdvertise = (id) => {
-        console.log(id)
+        setAdvertiseID(id)
+        //console.log('myitem', myitem);
+        fetch('http://localhost:5000/advertise', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                productName: advertise.productName,
+                price: advertise.price,
+                image: advertise.image,
+            })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                toast.success('data posted succesfuly')
+            })
+
+    }
+    const handleDeleteProducts = (id) => {
+        // console.log(advertiseId);
+        fetch(`http://localhost:5000/myProducts/${id}`, {
+            method: 'DELETE',
+            // headers: {
+            //     authorization: bearer ${localStorage.getItem('accessToken')}
+            // }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success('Product deleted successfully')
+                }
+            })
     }
 
     return (
         <div>
-            <h3 className='text-2xl text-[#B762C1] font-mono'>My products {products.length}</h3>
+            <h3 className='text-2xl text-[#B762C1] font-mono mb-5'>My products {products.length}</h3>
             <div className="overflow-x-auto">
                 <table className="table sm:w-1/4 lg:w-full">
                     <thead>
@@ -45,8 +92,8 @@ const MyProducts = () => {
                                 <td>{addproduct.productName}</td>
                                 <td>{addproduct.price}</td>
                                 <td>Status Not found</td>
-                                <td><button onClick={() => handleAdvertise(products._id)} className="btn btn-sm btn-success mt-3">Advertisement</button></td>
-                                <td> <button className="btn btn-sm btn-error mt-3">Delete</button></td>
+                                <td><button onClick={() => handleAdvertise((addproduct._id))} className="btn btn-sm btn-outline btn-secondary mt-3">Advertisement</button></td>
+                                <td><button onClick={() => handleDeleteProducts(addproduct._id)} className="btn btn-sm btn-outline btn-error mt-3">Delete</button></td>
 
                             </tr>)
                         }
